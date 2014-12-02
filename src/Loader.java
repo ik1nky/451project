fd3b825f64163a4c78361841a8b3b46e39311b7e
kinkemm1 <kinkemam@gmail.com>
2014-12-02 15:36:53 -0500

    just touched up

--- src/Loader.java
+++ src/Loader.java
@@ -1,4 +1,4 @@
-import com.sun.tools.doclets.internal.toolkit.util.DocFinder;
+//import com.sun.tools.doclets.internal.toolkit.util.DocFinder;
 import objects.*;
 
 import javax.swing.*;
@@ -27,142 +27,140 @@ public class Loader extends JPanel implements ActionListener {
         FileNameExtensionFilter filter = new FileNameExtensionFilter("JLS Files", new String[]{"jls"});
         fc.setFileFilter(filter);
         int returnValue = fc.showOpenDialog(main);
-        if (returnValue == 0){
-            File file;
-            String filename;
-            String circName;
-            try {
+        if (returnValue != 0){
+            System.out.println("Something went wrong");
+        }
 
-                file = fc.getSelectedFile();
-                filename = file.getName().trim();
-                FileInputStream input = new FileInputStream(file);
-                ZipInputStream circ = new ZipInputStream(input);
+        File file;
+        String filename;
+        String circName;
+        try {
 
-                Object in = null;
-                if (circ.getNextEntry() == null){
-                    input.close();
-                    in = new FileInputStream(file);
-                }else{
-                    in = circ;
-                }
+            file = fc.getSelectedFile();
+            filename = file.getName().trim();
+            FileInputStream input = new FileInputStream(file);
+            ZipInputStream circ = new ZipInputStream(input);
 
-                Scanner scan = new Scanner((InputStream) in);
-                if (!scan.next().equals("CIRCUIT")){
-                    System.out.println("invalid header");
-                    System.exit(1);
-                }else {
+            Object in = null;
+            if (circ.getNextEntry() == null){
+                input.close();
+                in = new FileInputStream(file);
+            }else{
+                in = circ;
+            }
 
-                    circName = scan.next();
+            Scanner scan = new Scanner((InputStream) in);
+            if (!scan.next().equals("CIRCUIT")){
+                System.out.println("invalid header");
+                System.exit(1);
+            }else {
 
-                    System.out.println("Circuit Name: " + circName);
-                    while (scan.next().equals("ELEMENT")) {
-                        //System.out.println(scan.nextLine());
-                        String componentType = scan.next();
-                        JLSElement element = getTypeObject(componentType);
-                        System.out.println("Class: " + element.getClass());
-                        String token;
+                circName = scan.next();
 
-                        while (!(token = scan.next()).equals("END")) {
-                            String dataType = token;
-                            String name = scan.next();
-                            String value = scan.next();
+                System.out.println("Circuit Name: " + circName);
+                while (scan.next().equals("ELEMENT")) {
+                    //System.out.println(scan.nextLine());
+                    String componentType = scan.next();
+                    JLSElement element = getTypeObject(componentType);
+                    System.out.println("Class: " + element.getClass());
+                    String token;
 
-                            switch (dataType){
-                                case "pair":
-                                    System.out.println("Setup a pair...");
+                    while (!(token = scan.next()).equals("END")) {
+                        String dataType = token;
+                        String name = scan.next();
+                        String value = scan.next();
 
-                            }
+                        switch (dataType){
+                            case "pair":
+                                System.out.println("Setup a pair...");
 
-                            switch (name.toLowerCase()){
-                                case "id":
-                                    element.setId(Integer.parseInt(value));
-                                case "x":
-                                    element.setX(Integer.parseInt(value));
-                                case "y":
-                                    element.setY(Integer.parseInt(value));
-                                case "width":
-                                    element.setWidth(Integer.parseInt(value));
-                                case "height":
-                                    element.setHeight(Integer.parseInt(value));
-                                case "name":
-                                    switch (componentType){
-                                        case "InputPin":
-                                            ((InputPin) element).setName(value);
-                                        case "OutputPin":
-                                            ((OutputPin) element).setName(value);
-                                        case "JumpStart":
-                                            ((JumpStart) element).setName(value);
-                                        case "JumpEnd":
-                                            ((JumpEnd) element).setName(value);
-                                        default:
-                                            System.out.println("Error when parsing name var: " + componentType);
-                                            System.exit(2);
-                                    }
-                                case "bits":
-                                    //splitter, jumpstart, jumpend, outputpin, inputpin,
-                                    switch (componentType){
-                                        case "InputPin":
-                                            ((InputPin) element).setBits(Integer.parseInt(value));
-                                        case "OutputPin":
-                                            ((OutputPin) element).setBits(Integer.parseInt(value));
-                                        case "JumpStart":
-                                            ((JumpStart) element).setBits(Integer.parseInt(value));
-                                        case "JumpEnd":
-                                            ((JumpEnd) element).setBits(Integer.parseInt(value));
-                                        case "Splitter":
-                                            ((Splitter) element).setBits(Integer.parseInt(value));
-                                        default:
-                                            System.out.println("Error when parsing bits var: " + componentType);
-                                            System.exit(2);
-                                    }
-                                case "delay":
-                                    ((Gate) element).setDelay(Integer.parseInt(value));
-                                case "orientation":
-                                    if (element instanceof Gate){
-                                        ((Gate) element).setOrientation(value);
-                                    }else if (element instanceof Jump){
-                                        ((Jump) element).setOrientation(value);
-                                    }
-                                case "numinputs":
-                                    ((Gate) element).setNumInputs(Integer.parseInt(value));
-                                case "tristate":
-                                    ((Splitter) element).setTristate(Integer.parseInt(value));
-                                case "noncontig":
-                                    ((Splitter) element).setNoncontig(value);
-                                case "orient":
-                                    if (element instanceof Splitter) {
-                                        ((Splitter) element).setOrient(value);
-                                    }else if (element instanceof InputPin){
-                                        ((InputPin) element).setOrient(value);
-                                    }else if (element instanceof OutputPin){
-                                        ((OutputPin) element).setOrient(value);
-                                    }else{
-                                        System.out.println("Error when parsing orient var: " + componentType);
-                                    }
-                                case "put":
-                                    ((WireEnd) element).setPut(value);
-                                case "ref":
-                                    if (name.equals("attach")){
-                                        ((WireEnd) element).addAttach(Integer.parseInt(value));
-                                    }else if (name.equals("wire")) {
-                                        ((WireEnd) element).addWire(Integer.parseInt(value));
-                                    }
+                        }
 
-                                default:
-                                    System.out.println("Problem parsing element. Component type: " + componentType + ", data type: " + dataType + ", name: " + name + " and value: " + value + ".");
-                                    System.exit(3);
+                        switch (name.toLowerCase()){
+                            case "id":
+                                element.setId(Integer.parseInt(value));
+                            case "x":
+                                element.setX(Integer.parseInt(value));
+                            case "y":
+                                element.setY(Integer.parseInt(value));
+                            case "width":
+                                element.setWidth(Integer.parseInt(value));
+                            case "height":
+                                element.setHeight(Integer.parseInt(value));
+                            case "name":
+                                switch (componentType){
+                                    case "InputPin":
+                                        ((InputPin) element).setName(value);
+                                    case "OutputPin":
+                                        ((OutputPin) element).setName(value);
+                                    case "JumpStart":
+                                        ((JumpStart) element).setName(value);
+                                    case "JumpEnd":
+                                        ((JumpEnd) element).setName(value);
+                                    default:
+                                        System.out.println("Error when parsing name var: " + componentType);
+                                        System.exit(2);
+                                }
+                            case "bits":
+                                //splitter, jumpstart, jumpend, outputpin, inputpin,
+                                switch (componentType){
+                                    case "InputPin":
+                                        ((InputPin) element).setBits(Integer.parseInt(value));
+                                    case "OutputPin":
+                                        ((OutputPin) element).setBits(Integer.parseInt(value));
+                                    case "JumpStart":
+                                        ((JumpStart) element).setBits(Integer.parseInt(value));
+                                    case "JumpEnd":
+                                        ((JumpEnd) element).setBits(Integer.parseInt(value));
+                                    case "Splitter":
+                                        ((Splitter) element).setBits(Integer.parseInt(value));
+                                    default:
+                                        System.out.println("Error when parsing bits var: " + componentType);
+                                        System.exit(2);
+                                }
+                            case "delay":
+                                ((Gate) element).setDelay(Integer.parseInt(value));
+                            case "orientation":
+                                if (element instanceof Gate){
+                                    ((Gate) element).setOrientation(value);
+                                }else if (element instanceof Jump){
+                                    ((Jump) element).setOrientation(value);
+                                }
+                            case "numinputs":
+                                ((Gate) element).setNumInputs(Integer.parseInt(value));
+                            case "tristate":
+                                ((Splitter) element).setTristate(Integer.parseInt(value));
+                            case "noncontig":
+                                ((Splitter) element).setNoncontig(value);
+                            case "orient":
+                                if (element instanceof Splitter) {
+                                    ((Splitter) element).setOrient(value);
+                                }else if (element instanceof InputPin){
+                                    ((InputPin) element).setOrient(value);
+                                }else if (element instanceof OutputPin){
+                                    ((OutputPin) element).setOrient(value);
+                                }else{
+                                    System.out.println("Error when parsing orient var: " + componentType);
+                                }
+                            case "put":
+                                ((WireEnd) element).setPut(value);
+                            case "ref":
+                                if (name.equals("attach")){
+                                    ((WireEnd) element).addAttach(Integer.parseInt(value));
+                                }else if (name.equals("wire")) {
+                                    ((WireEnd) element).addWire(Integer.parseInt(value));
+                                }
 
-                            }
-
+                            default:
+                                System.out.println("Problem parsing element. Component type: " + componentType + ", data type: " + dataType + ", name: " + name + " and value: " + value + ".");
+                                System.exit(3);
                         }
                     }
                 }
-
-            } catch (Exception e){
-                e.printStackTrace();
             }
-        }else{
-            System.out.println("Something went wrong");
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
