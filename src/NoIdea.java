5e1f6daf9ca0ec1e8f3e5829764e21b6583ee07e
Jack S. Rosenhauer II <jackrosenhauer@gmail.com>
2014-12-03 01:21:06 -0500

    Add test folder

--- src/NoIdea.java
+++ src/NoIdea.java
@@ -7,10 +7,11 @@ import java.io.InputStreamReader;
  */
 public class NoIdea {
     public static void main(String[] args){
-        String iHateYou = "/Users/jackrosenhauer/Dropbox/Git/451project/Test/ourScript.bash";
-        String[] myArray = {iHateYou};
+        String iHateYou = "/Users/jacksrosenhauerii/Dropbox/Git/451project/Test/ourScript.bash";
+        String[] myArray = {"/bin/sh", "-c", "Test/jlsCircuitTester -m 451 Files/fuck.jls Test/umm | grep \"Errors detected\" | awk -F\"-\" '{print $2}' | awk -F\":\" '{print $1}'"};
         try {
             Runtime rt = Runtime.getRuntime();
+            //Process p = rt.exec("Test/jlsCircuitTester -m 451 Files/fuck.jls Test/umm  \\| grep a");
             Process p = rt.exec(myArray);
             BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
@@ -20,7 +21,7 @@ public class NoIdea {
                 System.out.println(s);
             }
 
-            System.out.println(Runtime.getRuntime().exec(iHateYou).getOutputStream().toString());
+            //System.out.println(Runtime.getRuntime().exec(iHateYou).getOutputStream().toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
