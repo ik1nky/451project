28ac5b07c8ac1be9e353e76203490341973f5df9
Jack S. Rosenhauer II <jackrosenhauer@gmail.com>
2014-12-02 21:11:55 -0500

    Create toString methods for all needed objects

---
+++ src/Circuit.java
@@ -0,0 +1,13 @@
+import java.util.Hashtable;
+
+/**
+ * Created by jackrosenhauer on 12/2/14.
+ */
+public class Circuit {
+    String filename;
+    String circuitName;
+    Hashtable hashtable;
+
+
+
+}
